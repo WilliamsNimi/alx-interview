50b61db1f960907e47515a8a57c6459cd114ca30
Nimi Williams <williamson.nimi@gmail.com>
2024-03-10 09:48:06 +0100

    Fixed list of list error if n = 1

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -13,9 +13,9 @@ def pascal_triangle(n):
     if (n == 0):
         return ([])
     if (n == 1):
-        return ([1])
+        return ([[1]])
     if (n == 2):
-        return ([1, 1])
+        return ([[1, 1]])
     level2 = [1, 1]
     level3 = []
     overall_list = [[1], [1, 1]]
