c3a7940d37900975e7a7a8ae6e493f0c76efd333
Nimi Williams <williamson.nimi@gmail.com>
2024-05-01 17:44:26 +0100

    Implementing Greedy algorithm in python

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def makeChange(coins, total):
+    """ A Greedy algorithm to make change
+    @coins: currency denomination
+    @total: Total amount to be made into change
+    Return: Returns the total number of coins used or -1
+    """
+    num_change = 0
+    value_add = 0
+    coins.sort()
+    if total == 0 or total < 0:
+        return 0
+    for values in coins:
+        maxVal = coins[-1]
+        if total >= maxVal:
+            value_add = int(total / maxVal)
+            total = total - (value_add * maxVal)
+            num_change = num_change + value_add
+            coins.pop(-1)
+            if (total % maxVal) == 0:
+                return num_change
+        else:
+            coins.pop(-1)
+            if coins == []:
+                return -1
+            if (total % maxVal) != 0:
+                return -1
+
+    return -1
