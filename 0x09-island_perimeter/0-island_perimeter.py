ceb325c62418f7ec43dbe8ff8c766df2cac42c2f
Nimi Williams <williamson.nimi@gmail.com>
2024-05-09 21:50:33 +0100

    Algo revamp and adaptation to account for edge cases

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 """ Island perimeter module """
-def island_perimeter(grid) -> int:
-    """ Calculate the perimeter of an island """
-    count = 0
-    for i in grid:
-        for j in i:
-            if j == 1:
-                count+=1
+from typing import List
 
-    return (count * 2) + 2
+
+def island_perimeter(grid: List[List[int]]) -> int:
+    """ Calculate the perimeter of an island """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i < len(grid) - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 2
+
+                if j < len(grid) - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
