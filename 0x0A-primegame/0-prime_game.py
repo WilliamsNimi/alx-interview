e933a124eaac39c81db6a75a34ebc67b69a94aef
Nimi Williams <williamson.nimi@gmail.com>
2024-05-16 21:56:33 +0100

    First algorithm trial

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def isWinner(x, nums):
+    """ Prime game check for winner
+    @x: number of rounds to be played
+    @nums: The numbers to be generated
+    Return: Returns the winner of the game
+    """
+    maria = 0
+    ben = 0
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            maria += 1
+        else:
+            ben += 1
+
+    if maria > ben:
+        return "Maria"
+    else:
+        return "Ben"
---
+++ 0x0A-primegame/README.md
@@ -0,0 +1,1 @@
+Prima Game algorithm
