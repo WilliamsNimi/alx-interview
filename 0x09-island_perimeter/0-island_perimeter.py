2f462049ffc0ed8df08203b86f7a6b89904d8588
Nimi Williams <williamson.nimi@gmail.com>
2024-05-26 10:44:15 +0100

    Pycodestyle fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,31 +3,21 @@
 
 
 def island_perimeter(grid):
-    """ Calculate the perimeter of an island
+    """ Calculate the perimeter of an island"""
+    len_row = len(grid)
+    if (len_row > 0):
+        len_col = len(grid[0])
     perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid)):
-            if grid[i][j] == 1:
-                perimeter += 4
-                if i < len(grid) - 1 and grid[i + 1][j] == 1:
-                    perimeter -= 2
-
-                if j < len(grid) - 1 and grid[i][j + 1] == 1:
-                    perimeter -= 2 """
-    length_of_row = len(grid)
-    if (length_of_row > 0):
-        length_of_column = len(grid[0])
-    perimeter = 0
-    for row in range(length_of_row):
-        for column in range(length_of_column):
-            if grid[row][column] == 1:
-                if row == 0 or (row < length_of_row - 1 and grid[row+1][column] == 0):
+    for row in range(len_row):
+        for col in range(len_col):
+            if grid[row][col] == 1:
+                if row == 0 or (row < len_row - 1 and grid[row + 1][col] == 0):
                     perimeter += 1
-                if column == 0 or (column < length_of_column - 1 and grid[row][column+1] == 0):
+                if col == 0 or (col < len_col - 1 and grid[row][col + 1] == 0):
                     perimeter += 1
-                if row == length_of_row - 1 or (row > 0 and grid[row-1][column] == 0):
+                if row == len_row - 1 or (row > 0 and grid[row - 1][col] == 0):
                     perimeter += 1
-                if column == length_of_column - 1 or (column > 0 and grid[row][column - 1] == 0):
+                if col == len_col - 1 or (col > 0 and grid[row][col - 1] == 0):
                     perimeter += 1
 
     return perimeter
